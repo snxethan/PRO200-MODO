b33fece8f9c0afb91c8e57292763f07bb93d9537
JBrincefield <jbrincefield@student.neumont.edu>
2025-02-25 11:12:44 -0700

    created signup page and added nav between them
    title

--- shen-yun/src/app/signUp/page.tsx
+++ shen-yun/src/app/signUp/page.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import { useAuth } from "@/lib/AuthProvider";
 import { useRouter } from "next/navigation";
@@ -7,15 +7,9 @@ import { Button } from "@/components/ui/button";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
-export default function LoginPage() {
-    const { signIn, user } = useAuth();
-    const router = useRouter();
 
-    useEffect(() => {
-        if (user) {
-            router.push("/");
-        }
-    }, [user, router]);
+
+export default function SignUpPage() {
 
     return (
         <div className="bg-[#202C39] min-h-screen flex flex-col items-center p-6">
@@ -24,11 +18,9 @@ export default function LoginPage() {
                 <Navbar/>
             </div>
             
-            
-
-            {/* Login Form */}
+            {/* Sign Up Form */}
             <div className="bg-gray-700 p-8 mt-10 rounded-xl shadow-md w-[400px] text-center">
-                <h2 className="text-[#FAFFEB] text-lg mb-4">Login</h2>
+                <h2 className="text-[#FAFFEB] text-lg mb-4">Sign Up</h2>
 
                 <div className="text-left text-[#FAFFEB] mb-2">Email</div>
                 <input
@@ -44,25 +36,24 @@ export default function LoginPage() {
                     defaultValue={""}
                 />
 
+                <div className="text-left text-[#FAFFEB] mb-2">Confirm Password</div>
+                <input
+                    type="password"
+                    className="w-full p-2 rounded-md mb-4 bg-white text-black"
+                    defaultValue={""}
+                />
+
                 <Button
-                    onClick={signIn}
                     className="bg-[#ED7E07] text-[#FAFFEB] w-full py-2 rounded-full mb-4"
                 >
-                    &lt;Sign In with Google&gt;
+                    &lt;Sign Up with Google&gt;
                 </Button>
 
-                <Button
-                    variant="outline"
-                    className="border-[#ED7E07] text-ivory w-full mb-4"
-                >
-                    Login
-                </Button>
-
-                <Link href="/signUp">
+                <Link href="/login">
                     <Button
                         variant="outline"
                         className="border-[#ED7E07] text-ivory w-full mb-4">
-                        Don't have an account? Sign Up Today!
+                        Already have an account? Log In!
                     </Button>
                 </Link>
             </div>
